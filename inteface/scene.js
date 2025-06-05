36ee801eb886c39c98bf8590afac2334bb3c800c
UwaweTera <uwaweteraviv@gmail.com>
2025-06-05 12:36:55 +0200

    add climate

--- inteface/scene.js
+++ inteface/scene.js
@@ -11,24 +11,48 @@ function drawSky()
 {
     glEnableFog = 0; // disable fog
 
-    //fogColor = hsl(200,.5,.5,0);  
-    glSetDepthTest(0);
-
-    {
-        // horizon
-        // background
-        let s = 1e4;
-        pushGradient(vec3(0,s,1e4), vec3(5e4,s), WHITE,  hsl(.57,1,.5));
-        //pushGradient(vec3(0,s,1e4), vec3(5e4,s), RED,  RED);
-        //pushGradient(vec3(0,-s+2e3,1e4), vec3(s), hsl(0,0,0),  hsl(0,1,0));
+    // Set sky color based on weather
+    let skyColor, horizonColor;
+    if (currentWeather === 'rain') {
+        skyColor = hsl(200, 0.3, 0.3); // Dark blue-gray for rain
+        horizonColor = hsl(200, 0.2, 0.4); // Slightly lighter at horizon
+    } else {
+        skyColor = hsl(200, 0.5, 0.5); // Normal blue sky
+        horizonColor = hsl(0.57, 1, 0.5); // Normal horizon color
     }
 
-    // clouds
+    {
+        // horizon and background
+        let s = 1e4;
+        pushGradient(vec3(0,s,1e4), vec3(5e4,s), WHITE, horizonColor);
+    }
+
+    // Draw clouds with weather-based modifications
     random.setSeed(13);
     for(let i=99;i--;)
     {
         let s = random.float(900,2000);
-        pushSprite(vec3(worldHeading*-3e4 + random.floatSign(1e5),random.float(2000,9000),1e4), vec3(s*3,s,s), hsl(.15,1,.95,.5), getGenerativeTile(vec3(1,0)))
+        let cloudColor;
+        
+        if (currentWeather === 'rain') {
+            // Darker, more ominous clouds for rain
+            cloudColor = hsl(0.15, 0.3, 0.4, 0.7);
+        } else {
+            // Normal white clouds for sunny weather
+            cloudColor = hsl(0.15, 1, 0.95, 0.5);
+        }
+        
+        pushSprite(vec3(worldHeading*-3e4 + random.floatSign(1e5),random.float(2000,9000),1e4), 
+                  vec3(s*3,s,s), cloudColor, getGenerativeTile(vec3(1,0)));
+    }
+
+    // Draw rain particles if weather is rain
+    if (currentWeather === 'rain') {
+        for(let i = rainParticles.length; i--;) {
+            let p = rainParticles[i];
+            pushSprite(vec3(p.x, p.y, p.z), vec3(2, 20, 1), 
+                      hsl(200, 0.5, 0.8, 0.6), getGenerativeTile(vec3(1,0)));
+        }
     }
 
     glRender();
